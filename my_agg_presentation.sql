3b1a764cde4a2964e2cdfacd6151064133cb36f8
Georgios Kokolatos <gkokolatos@pivotal.io>
2020-02-28 15:37:13 +0000

    Update the hypothesis for the aggregate
    Instead of figuring the step which frankly can happen with a single SQL
    expression (value - lag(value, 1) over w) without even bothering with a CTE try
    to set up a rather convoluted scenario that expects three column outputs.
    
    Commulative sum, step (defined as current value - previous value), and running
    max step (defined as the overall maximum step until now, unless now is the
    overall maximum)
    
    Indeed it is a rather contrived example, though it requires to
    a) perform the calculation of the cummulative sum (which means we have to
    remember the previous value)
    b) do a rather simple currnet value - previous (NON NULL) value
    c) and note and update the current max of b)
    
    To do all this we still use an array as a state, we do need to take into account
    that the first value might not be null so we need to update it the first time,
    i.e. the initial condition for the previous value needs to be NULL. If it were 0
    and the first random value was negative the first step would be negative value
    instead of 0.
    
    Also update the awkward float8 to double precision.

--- my_agg_presentation.sql
+++ my_agg_presentation.sql
@@ -2,7 +2,7 @@
 -- create a table with some data
 CREATE TABLE aggregate_test (
 	id	SERIAL PRIMARY KEY,
-	value FLOAT8 DEFAULT NULL
+	value double precision DEFAULT NULL
 );
 
 -- insert some reproducible random-ish values
@@ -34,76 +34,98 @@ ORDER BY
 ASC;
 
 -- great, and the step?
-WITH cte AS (SELECT
-    id,
-    value,
-    sum(value) OVER (ORDER BY id ASC) AS "Cummulative Sum",
-    LAG(value, 1) OVER (ORDER BY id ASC) AS lag
+SELECT
+	id,
+	value,
+	sum(value) OVER w AS "Cummulative Sum",
+	value - LAG(value, 1) OVER w AS step
 FROM
-    aggregate_test
-ORDER BY
-    id
-ASC)
-SELECT id, value, "Cummulative Sum", value - lag AS step FROM cte;
+	aggregate_test
+WINDOW w AS (ORDER BY id ASC)
+ORDER BY id ASC;
+
+-- what about preserving the max step
+-- each row should return the already max step,
+-- or the new step if it is max
+-- This is not IT
+--WITH cte AS (SELECT
+--    id,
+--    value,
+--    sum(value) OVER (ORDER BY id ASC) AS "Cummulative Sum",
+--    LAG(value, 1) OVER (ORDER BY id ASC) AS lag
+--FROM
+--    aggregate_test
+--ORDER BY
+--    id
+--ASC)
+--SELECT id, value, "Cummulative Sum", value - lag AS step FROM cte;
 
 -- transition function NOTE arrays are 1 based
-CREATE OR REPLACE FUNCTION our_custom_transition(state float8[], value float8)
-        RETURNS float8[] IMMUTABLE
+CREATE OR REPLACE FUNCTION our_custom_transition(state double precision[], value double precision)
+        RETURNS double precision[] IMMUTABLE
         LANGUAGE plpgsql AS
 $$
 DECLARE
-        cummulative_sum float8;
-        difference              float8;
+        cummulative_sum double precision;
+        difference              double precision;
 BEGIN
         IF value IS NULL THEN
                 cummulative_sum := state[1];
                 difference := state[3];
         ELSE
+				IF state[3] IS NULL THEN
+					state[3] := value;
+				END IF;
                 cummulative_sum := state[1] + value;
                 difference :=  value - state[3];
                 state[3] := value;
         END IF;
 
-        RETURN array[cummulative_sum, difference, state[3]];
+		IF difference >= state[4] THEN
+			state[4] := difference;
+		END IF;
+        RETURN array[cummulative_sum, difference, state[3], state[4]];
 END;
 $$;
 
 -- final function, i.e. pretty print
-CREATE OR REPLACE FUNCTION our_custom_final(state float8[])
-        RETURNS float8[] IMMUTABLE
+CREATE OR REPLACE FUNCTION our_custom_final(state double precision[])
+        RETURNS double precision[] IMMUTABLE
         LANGUAGE plpgsql AS
 $$
 BEGIN
-  RETURN array[state[1], state[2]];
+  RETURN array[state[1], state[2], state[4]];
 END;
 $$;
 
 -- This is the aggregate
-CREATE OR REPLACE aggregate our_custom_aggregate(float8) (
-	initcond='{0.0, 0.0, 0.0}',
+CREATE OR REPLACE aggregate our_custom_aggregate(double precision) (
+	initcond='{0.0, 0.0, NULL, 0.0}',
 	sfunc = our_custom_transition,
 	finalfunc = our_custom_final,
-	stype = float8[]
+	stype = double precision[]
 );
 
 -- And?
-SELECT
-	id,
-	value,
-	our_custom_aggregate(value) OVER (order by id asc)
-FROM
-	aggregate_test;
+--SELECT
+--	id,
+--	value,
+--	our_custom_aggregate(value) OVER (order by id asc)
+--FROM
+--	aggregate_test;
 
 -- hack hack for getting columns back, a complex type would be better in this
 -- case
-CREATE OR REPLACE FUNCTION pretify (state float8[])
-	RETURNS TABLE ("Cummulative Sum" float8, step float8)
-	LANGUAGE sql AS
+CREATE OR REPLACE FUNCTION pretify (state double precision[])
+	RETURNS TABLE ("Cummulative Sum" double precision, step double precision,
+"Running max step" double precision)
+	LANGUAGE sql IMMUTABLE AS
 $$
-	SELECT state[1], state[2];
+	SELECT state[1], state[2], state[3];
 $$
 ;
 
+-- this will create columns
 SELECT
 	id,
 	value,
